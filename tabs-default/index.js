525c453773cc92e667df8cb9c4317362e59b2bf0
Welliton Gervickas <welliton@send4.com.br>
2018-10-09 22:20:11 -0300

    fix tabs component

--- tabs-default/index.js
+++ tabs-default/index.js
@@ -4,61 +4,49 @@ class TabsDefault extends Component {
 
   state = {
     currentTab: '',
+    itens: [],
   }
 
   componentWillMount() {
-    if (this.props.children && Array.isArray(this.props.children)) {
+    if (this.props.children) {
 
+      let getListOrItem = Array.isArray(this.props.children) ? [ ...this.props.children ] : [ this.props.children ];
+      let itens = getListOrItem.filter(item => (item.key && item.props.show !== false));
 
-      let tabs = this.props.children;
-      let defaultTab = tabs.find(item => item.props.defaultTab === true)
-      this.setState({ currentTab: defaultTab ? defaultTab.key : tabs[0].key });
+      if (itens && itens.length) {
+        let defaultTab = itens.find(item => item.props.defaultTab === true);
+        let currentTab = defaultTab ? defaultTab.key : itens[0].key;
 
-    } else {
-      this.setState({ currentTab: this.props.children.key })
+        // Update Itens List
+        this.setState({ itens, currentTab });
+      }
     }
   }
 
-  renderTabs(itens) {
-    return (
-      <div className={ 'tabs-default' + (this.props.className ? ' ' + this.props.className : '') }>
-
-        <ul className="tabs-options unstyled-list">
-          {itens.length > 1 ? itens.map(item => (
-            item.props.show !== false &&
-            <li
-              key={ item.key }
-              className={ this.state.currentTab === item.key ? 'active' : 'inactive' }
-              onClick={ () => this.setState({ currentTab: item.key })}>
-              { item.props.title }
-            </li>
-            )) : (
-            this.props.children.props.show !== false &&
-            <li
-              key={ this.props.children.key }
-              className={ this.state.currentTab === this.props.children.key ? 'active' : 'inactive' }
-              onClick={ () => this.setState({ currentTab: this.props.children.key })}>
-              { this.props.children.props.title }
-            </li>
-          )}
-        </ul>
-
-        {itens.length > 1 ? itens.map(item => (
-          <div key={ item.key } className={'tabs-item' + ( this.state.currentTab === item.key ? ' show' : ' hide') }>
-            { item }
-          </div>
-          )) : (
-          <div key={ this.props.children.key } className={'tabs-item' + ( this.state.currentTab === this.props.children.key ? ' show ' : ' hide') }>
-            { this.props.children }
-          </div>
-        )}
-
-      </div>
-    )
-  }
-
   render() {
-    return this.renderTabs([...this.props.children]);
+    if (this.state.itens && this.state.itens.length) {
+      return (
+        <div className={ 'tabs-default' + (this.props.className ? ' ' + this.props.className : '') }>
+          <ul className="tabs-options unstyled-list">
+            { this.state.itens.map(item =>(
+              <li
+                key={ item.key }
+                className={ this.state.currentTab === item.key ? 'active' : 'inactive' }
+                onClick={ () => this.setState({ currentTab: item.key })}>
+                { item.props.title }
+              </li>
+            ))}
+          </ul>
+          {this.state.itens.map(item => (
+            <div key={ item.key } className={'tabs-item' + ( this.state.currentTab === item.key ? ' show' : ' hide') }>
+              { item }
+            </div>
+          ))}
+        </div>
+      )
+    }
+
+    return null
   }
 };
 
